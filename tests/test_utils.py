0001acc453bb9c0d65102e5e8ff2ef7041f7c8dc
Julien Leicher <jleicher@gmail.com>
2018-12-13 21:14:12 +0100

    Added some more tests to deep_update

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -31,13 +31,22 @@ class TestUtils:
             },
           ],
           'automatically_extensible': False,
-        }
+        },
+        'city': {
+          'data': [
+            {
+              'value': 'paris',
+            },
+          ],
+          'automatically_extensible': True,
+        },
       }
     }
 
     result = deep_update(a, b)
 
     expect(result['language']).to.equal('fr')
+    expect(result['entities']).to.have.key('room')
     expect(result['entities']['room']['automatically_extensible']).to.be.false
 
     data = result['entities']['room']['data']
@@ -49,3 +58,7 @@ class TestUtils:
     expect(room_names).to.contain('kitchen')
     expect(room_names).to.contain('bedroom')
     expect(room_names).to.contain('living room')
+
+    expect(result['entities']).to.have.key('city')
+    expect(result['entities']['city']['data']).to.have.length_of(1)
+    expect(result['entities']['city']['automatically_extensible']).to.be.true
